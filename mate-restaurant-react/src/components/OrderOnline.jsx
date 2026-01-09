3cd0bbfe7242e50e5bfad5961b41be8e854e5516
Navida Ninduwara <navidaninduwaraofficial@gmail.com>
2026-01-09 20:28:51 +0530

    Update OrderOnline component and styling

--- mate-restaurant-react/src/components/OrderOnline.jsx
+++ mate-restaurant-react/src/components/OrderOnline.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import AnimatedSection from './AnimatedSection';
 import './OrderOnline.css';
 
 const OrderOnline = () => {
     return (
         <section className="order-online">
             <div className="container">
-                <h2>Enjoy Mate at Home</h2>
-                <p>Order now through our delivery partners</p>
-                <div className="order-buttons">
-                    <a href="#" className="btn-outline">
-                        Uber Eats
-                    </a>
-                    <a href="#" className="btn-outline">
-                        PickMe Food
-                    </a>
-                </div>
+                <AnimatedSection animationType="fade-up">
+                    <h2>Enjoy Mate at Home</h2>
+                    <p>Order now through our delivery partners</p>
+                    <div className="order-buttons">
+                        <a href="#" className="btn-outline">
+                            Uber Eats
+                        </a>
+                        <a href="#" className="btn-outline">
+                            PickMe Food
+                        </a>
+                    </div>
+                </AnimatedSection>
             </div>
         </section>
     );
